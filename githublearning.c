faf3924fecbc1367a5d3b1cff54dc8fcfe169d99
autobahnotoban <arabaciatahan@gmail.com>
2024-02-09 22:48:20 +0300

    added more to c code to test

--- githublearning.c
+++ githublearning.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
+
+
+void printScreen ()
+{
+    printf(" Hello! Welcome! \n");
+    printf("Here you can do your calcs.\n");
+}
+
 int main()
 {
-    printf("Hello");
+    printScreen();
 }
